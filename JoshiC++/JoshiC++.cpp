4023a192df0ee231e77c97ca45c87b0e3a777f3f
bwrob <43605200+bwrob@users.noreply.github.com>
2022-08-20 23:50:14 +0200

    Verbatim dummy implementation from Ch1

--- JoshiC++/JoshiC++.cpp
+++ JoshiC++/JoshiC++.cpp
@@ -1,20 +1,77 @@
-﻿// JoshiC++.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿#include"Random.h"
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+//function doing the MC simulation
+double SimpleMonteCarlo1(double Expiry,
+    double Strike,
+    double Spot,
+    double Vol,
+    double r,
+    unsigned long NumberOfPath)
+{
+    double variance = Vol * Vol * Expiry;
+    double rootVariance = sqrt(variance);
+    double itoVorrelation = -0.5 * variance;
+
+    double movedSpot = Spot * exp(r * Expiry + itoVorrelation);//compute first to recude call exp()
+    double thisSpot;
+    double runningSum = 0;
+
+    for (unsigned long i = 0; i < NumberOfPath; i++)
+    {
+        double thisGaussian = GetOneGaussianByBoxMuller();
+        thisSpot = movedSpot * exp(rootVariance * thisGaussian);
+        double thisPayoff = thisSpot - Strike;
+        thisPayoff = thisPayoff > 0 ? thisPayoff : 0;
+        runningSum += thisPayoff;
+    }
+
+    double mean = runningSum / NumberOfPath;
+    mean *= exp(-r * Expiry);
+    return mean;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double Expiry;
+    double Strike;
+    double Spot;
+    double Vol;
+    double r;
+    unsigned long NumberOfPath;
+
+    //read in parameters
+    cout << "\nEnter expiry\n";
+    cin >> Expiry;
+
+    cout << "\nEnter Strike\n";
+    cin >> Strike;
+
+    cout << "\nEnter spot\n";
+    cin >> Spot;
+
+    cout << "\nEnter vol\n";
+    cin >> Vol;
+
+    cout << "\nEnter r\n";
+    cin >> r;
+
+    cout << "\n Number of paths\n";
+    cin >> NumberOfPath;
+
+    double result = SimpleMonteCarlo1(Expiry,
+        Strike,
+        Spot,
+        Vol,
+        r,
+        NumberOfPath);
+    cout << "the price is " << result << "\n";
+
+    double tmp;
+    cin >> tmp;
+
+    return 0;
+
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
